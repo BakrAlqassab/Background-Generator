0db558077b3e27e9ec6059843245f0496d774c68
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2021-02-01 17:22:40 +0200

    refactor the code and make the project show the selected color to copy it when get the perfect background and add the ability of modify the text Color

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.getElementById('gradient');
-
+var textColor = document.querySelector(".textColor");
+var h1 = document.querySelector("h1");
+var h2 = document.querySelector("h2");
+var body = document.getElementById("gradient");
 
 function setGradient() {
-      body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value}) `
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value}) `;
+  css.textContent = body.style.background + ";"
 }
-const color1Fuc = () => {
-setGradient();
 
-};
+function setTextColor() {
+  h1.style.color =textColor.value;
+  h2.style.color =textColor.value;
+}
 
-const color2Fuc = () => {
- setGradient();
-};
-color1.addEventListener("input", color1Fuc);
-color2.addEventListener("input", color2Fuc);
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+textColor.addEventListener("input", setTextColor);
